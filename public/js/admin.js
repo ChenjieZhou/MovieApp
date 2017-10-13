6a093170288a051982a532945aa70998f71dec24
Chenjie Zhou <zhouchenjie@outlook.com>
2017-10-12 22:27:21 -0700

    Update admin.js

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,21 +1,4 @@
-// $(function(){
-//   $('.del').click(function(e){
-//     var target = $(e.target);
-//     var id = target.data('id');
-//     var tr = $('.item-id' + id);
-//
-//     $ajax({
-//       type: 'DELETE',
-//       url: '/admin/list?id' + id;
-//     }).done(function(results) {
-//       if (results.success === 1) {
-//         if (tr.length > 0) {
-//           tr.remove();
-//         }
-//       }
-//     })
-//   })
-// })
+
 
 $(function() {
     // body...
@@ -38,24 +21,23 @@ $(function() {
 
     $('#douban').blur(function() {
         var douban = $(this)
-				console.log(douban);
+        // console.log(douban);
         var id = douban.val()
 
+        if (id){
+		$.ajax({
+			url: 'https://api.themoviedb.org/3/movie/'+id +'/videos?api_key=50f5bf470407f34dbf7c67c165eff65b&language=en-US',
+			cache: true,
+			type: 'get',
+			dataType: 'json',
+			success: function(data) {
+				var trailerKey = data.results[0].key;
+				var trailer = 'http://www.youtube.com/embed/'+trailerKey;
+				$('#inputFlash').val(trailer);
 
-				if (id){
-					$.ajax({
-							url: 'https://api.themoviedb.org/3/movie/'+id +'/videos?api_key=50f5bf470407f34dbf7c67c165eff65b&language=en-US',
-							cache: true,
-							type: 'get',
-							dataType: 'json',
-							success: function(data) {
-								var trailerKey = data.results[0].key;
-								var trailer = 'http://www.youtube.com/embed/'+trailerKey;
-								$('#inputFlash').val(trailer);
-
-							}
-					});
-				}
+			}
+		});
+	}
 
         if (id) {
             $.ajax({
@@ -64,9 +46,9 @@ $(function() {
                 type: 'get',
                 dataType: 'json',
                 success: function(data) {
-										console.log(data+"this is ceshi");
+		console.log(data+"this is ceshi");
                     $('#inputTitle').val(data.Title);
-										  $('#inputLanguage').val(data.Language);
+			$('#inputLanguage').val(data.Language);
                     $('#inputDoctor').val(data.Director);
                     $('#inputCountry').val(data.Country);
                     $('#inputPoster').val(data.Poster);
